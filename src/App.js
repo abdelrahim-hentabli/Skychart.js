e8f82601a34dba0df1047ef61d26e2726497bd76
Abdelrahim Hentabli <abdelrahim.hentabli@gmail.com>
2021-03-13 15:32:52 -0800

    fixed header and footer of site

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import logo from './logo.svg';
+import github_logo from './GitHub-Mark-32px.png';
+import linkedin_logo from './LI-In-Bug.png';
+import portfolio_logo from './8707701631578981545-512.png'
 import './App.css';
 
 function App() {
@@ -7,17 +10,43 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Skychart
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body className="App-body">
+        Hello There
+      </body>
+      <footer
+          className="App-footer"
+        >
+          <a
+            className="App-link"
+            href="https://github.com/abdelrahim-hentabli/skychart.js"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={github_logo} className="App-icons" />
+              GitHub
+          </a>
+          <a
+            className="App-link"
+            href="https://www.linkedin.com/in/abdelrahim-hentabli/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedin_logo} className="App-icons" />
+            Linkedin
+          </a>
+          <a
+            className="App-link"
+            href="https://github.com/abdelrahim-hentabli"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={portfolio_logo} className="App-icons" />
+              Portfolio
+          </a>
+        </footer>
     </div>
   );
 }

199e878157532fd745cc251922868e5d1dc6bf6a
Abdelrahim Hentabli <abdelrahim.hentabli@gmail.com>
2021-04-08 00:25:05 -0700

    created csv reader with creation of star objects

---
+++ src/csvreader.js
@@ -0,0 +1,21 @@
+import stars from './topstars.js'
+import Star from './star.js'
+
+import {currentTime, currentLocation} from './globalvars.js'
+
+
+var starArray = [];
+
+function parseFile(){
+    var starsStringArray = stars.split('\n');
+    var currentLine;
+    
+    for(var i = 0; i < starsStringArray.length; i++){
+        currentLine = starsStringArray[i].split(',');
+        starArray.push(new Star(parseInt(currentLine[0]), currentLine[1], currentLine[2], parseFloat(currentLine[3])+ parseFloat(currentLine[4])/60, parseFloat(currentLine[5]), parseFloat(currentLine[6])));
+        starArray[i].update(currentLocation, currentTime);
+        //console.log("Altitude: " + starArray[i].altitude + ",  Azimuth: " + starArray[i].azimuth);
+    }
+}
+
+export default parseFile

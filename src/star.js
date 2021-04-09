5a079c73d8d745dcda78fd198e0acde6256565d3
Abdelrahim Hentabli <abdelrahim.hentabli@gmail.com>
2021-04-08 17:15:15 -0700

    fixed parenthesis issue

--- src/star.js
+++ src/star.js
@@ -21,11 +21,11 @@ class Star{
         var longitude = coordinate.getLongitude();
         var latitude = coordinate.getLatitude();
         var LHA = time.getSiderealTime() + (longitude * 24.0) / 360.0 - this.rightAscension;
-        if(LHA < 0 ){
+        if(LHA < 0){
             LHA += 24;
         }
         //TODO: Check the next two lines and make them more readable
-        this.altitude = this.toDegrees(Math.asin((Math.sin(this.toRadians(latitude)) * Math.sin(this.toRadians(this.declination))) + (Math.cos(this.toRadians(latitude)) * Math.cos(this.toRadians(this.declination)) * Math.cos(this.toRadians(LHA / 24.0) * 360))));
+        this.altitude = this.toDegrees(Math.asin((Math.sin(this.toRadians(latitude)) * Math.sin(this.toRadians(this.declination))) + (Math.cos(this.toRadians(latitude)) * Math.cos(this.toRadians(this.declination)) * Math.cos(this.toRadians((LHA / 24.0) * 360)))));
         this.azimuth = this.toDegrees(Math.acos((Math.sin(this.toRadians(this.declination)) - (Math.sin(this.toRadians(latitude)) * Math.sin(this.toRadians(this.altitude)))) / (Math.cos(this.toRadians(latitude)) * Math.cos(this.toRadians(this.altitude)))));
         if(LHA > 0  && LHA < 12){
             this.azimuth = 360 - this.azimuth;
